ea0a93947e358797efb62b28b4fe9915fc29d5c2
keunhyouk.park <keunhyouk.park@firsthabit.com>
2026-02-02 00:26:27 +0900

    feat: 최신 사양 반영 - Spring Boot 3.5.7, AWS SDK 2.37.3 업그레이드 - JSch를 Apache MINA SSHD 2.13.2로 교체 - CategoryType에 LECTURE, CHAPTER, QUESTION, QUIZ, UNIVERSITY 추가 - SignedFileResponse에 fileKey 필드 및 단일 인자 생성자 추가 - AdminFileAccessController, AdminFileUploadController 추가 - SshTunneling을 Apache MINA SSHD로 완전 재구현 - 민감 정보를 환경변수로 외부화 (S3, Redis, JWT, Admin Key) - SSH 설정을 환경변수 기반으로 변경
    (cherry picked from commit dbdadc7af42b02df8e4350ab79d8b15d55211ae6)

--- src/main/java/com/endside/file/manage/response/SignedFileResponse.java
+++ src/main/java/com/endside/file/manage/response/SignedFileResponse.java
@@ -1,16 +1,25 @@
 package com.endside.file.manage.response;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class SignedFileResponse {
     private String url;
+    @JsonInclude(NON_NULL)
+    private String fileKey;
 
-    public SignedFileResponse(String url){
+    /**
+     * URL만으로 생성하는 생성자 (기존 코드 호환성 유지)
+     */
+    public SignedFileResponse(String url) {
         this.url = url;
+        this.fileKey = null;
     }
 }
